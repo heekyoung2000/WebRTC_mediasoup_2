d859ce5c68a31084b3e5c07199b82a159480ccac
developer_jennifer <heekyoung2000@naver.com>
2023-07-20 21:24:01 +0900

    Update host
    Update host

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const port = 3000; //3000번 포트
-const host = '0.0.0.0';
+const host = '43.201.47.117';
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World!')
