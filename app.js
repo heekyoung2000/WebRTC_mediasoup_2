7e13cc36966f0ac664b447997b8a353cb7b63138
heekyoung2000 <heekyoung2000@naver.com>
2023-07-24 14:13:16 +0900

    room 인원 제한

--- app.js
+++ app.js
@@ -382,9 +382,9 @@ const createWebRtcTransport = async (router) => {
             const webRtcTransport_options = {
                 listenIps: [
                     {
-                        ip: '172.31.5.109', // replace with relevant IP address
+                        ip: '0.0.0.0', // 172.31.5.109replace with relevant IP address
 
-                        announcedIp: '43.201.47.117',
+                        announcedIp: '127.0.0.1',//43.201.47.117
                     }
                 ],
                 enableUdp: true,
